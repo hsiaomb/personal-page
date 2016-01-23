49e7931dcc213866d6aaf4bc1ee81fca99768939
hsiaomb <hsiaomb@usc.edu>
2016-01-23 14:05:38 -0500

    Added index.php

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$(document)
+.ready(function() {
+  $('.main.menu').visibility({
+         type: 'fixed'
+       });
+  // fix menu when passed
+  $('.masthead')
+  .visibility({
+    once: false,
+    onBottomPassed: function() {
+      $('.fixed.menu').transition('fade in');
+    },
+    onBottomPassedReverse: function() {
+      $('.fixed.menu').transition('fade out');
+    }
+  });
+
+  $('.ui.modal')
+  .modal('attach events', '.contact.item', 'show');
+
+  $('.ui.modal')
+  .modal('attach events', '.contact-footer', 'show');
+
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 46
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+  $('#fullpage').fullpage({
+		autoScrolling: false,
+		fitToSection: false
+	});
+});
