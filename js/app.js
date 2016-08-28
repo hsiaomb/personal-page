1610a8d7afd86686e5020c1ec9f587385e13b7fb
Michael Hsiao <hsiaomb@usc.edu>
2016-08-27 23:51:27 -0700

    alskdjf'

--- js/app.js
+++ js/app.js
@@ -1,50 +1,17 @@
-$(document)
-.ready(function() {
-  $('.main.menu').visibility({
-         type: 'fixed'
-       });
-  // fix menu when passed
-  $('.masthead')
-  .visibility({
-    once: false,
-    onBottomPassed: function() {
-      $('.fixed.menu').transition('fade in');
-    },
-    onBottomPassedReverse: function() {
-      $('.fixed.menu').transition('fade out');
-    }
-  });
+// app.js
+var routerApp = angular.module('routerApp', ['ui.router']);
 
-  $('.ui.modal')
-  .modal('attach events', '.contact.item', 'show');
-
-  $('.ui.modal')
-  .modal('attach events', '.contact-footer', 'show');
-
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top - 44
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-
-  $('#fullpage').fullpage({
-		autoScrolling: false,
-		fitToSection: false
-	});
-
-  $('#fullpage').imagesLoaded( function() {
-    $('#webdev-title').removeClass('hidden');
-    $('#webdev-title').addClass('fadeIn');
-    $('#name-title').removeClass('hidden');
-    $('#name-title').addClass('fadeIn');
-  $('#continue').removeClass('hidden');
-  $('#continue').addClass('fadeIn');
-});
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: '../partials/homepage.html'
+        })
+        
+        
 });
